eee645c5eaaa43c3ad9bfefb2ed81c5adeca4aae
Marc Bryan <marc2982@gmail.com>
2025-12-03 19:45:56 -0700

    Extract calculation functions to an object

--- playoffs/js/csvProcessor.js
+++ playoffs/js/csvProcessor.js
@@ -6,6 +6,7 @@ import { Summarizer } from './summarizer.js';
 import { ProjectionCalculator } from './projectionCalculator.js';
 import { fetchText } from './httpUtils.js';
 import { DataLoader } from './dataLoader.js';
+import { PickResultCalculator } from './pickResultCalculator.js';
 
 export async function loadData(year) {
 	try {
@@ -39,6 +40,7 @@ async function loadAndProcessCsvs(year) {
 	await api.load();
 
 	const picksImporter = new PicksImporter(api);
+	const calculator = new PickResultCalculator();
 	const summarizer = new Summarizer(year, api.getTeams());
 	const projector = new ProjectionCalculator(api);
 	const rounds = [];
@@ -49,7 +51,7 @@ async function loadAndProcessCsvs(year) {
 		const seriesLetters = ALL_SERIES[roundNum - 1];
 		const serieses = seriesLetters.map((letter) => api.getSeries(letter));
 		const picks = await picksImporter.readCsv(`./data/${year}`, roundNum);
-		const pickResults = buildPickResults(scoring, api, picks);
+		const pickResults = calculator.buildPickResults(scoring, api, picks);
 		const summary = summarizer.summarizeRound(pickResults);
 		rounds.push(
 			Round.create({
@@ -75,81 +77,4 @@ export async function loadCsv(filename) {
 		console.error(`Failed to load CSV file ${filename}:`, err);
 		throw err;
 	}
-}
-
-function buildPickResults(scoring, api, picks) {
-	const pickResults = {};
-	for (const [person, picksBySeries] of Object.entries(picks)) {
-		for (const [seriesLetter, pick] of Object.entries(picksBySeries)) {
-			const series = api.getSeries(seriesLetter);
-			const winner = series.getWinner();
-			const teamStatus = getTeamStatus(pick, winner);
-			const gamesStatus = getGamesStatus(pick, winner, series);
-			const points = getPoints(scoring, teamStatus, gamesStatus);
-			const possiblePoints = winner
-				? points
-				: calculatePossiblePoints(pick, series, scoring, teamStatus, gamesStatus);
-			if (!pickResults[person]) {
-				pickResults[person] = {};
-			}
-			pickResults[person][series.letter] = PickResult.create({
-				pick: pick,
-				teamStatus: teamStatus,
-				gamesStatus: gamesStatus,
-				points: points,
-				possiblePoints: possiblePoints,
-			});
-		}
-	}
-	return pickResults;
-}
-
-function getPickStatus(pick, winner, predicate) {
-	if (!winner) {
-		return PickStatus.UNKNOWN;
-	}
-	return predicate(pick, winner) ? PickStatus.CORRECT : PickStatus.INCORRECT;
-}
-
-function getTeamStatus(pick, winner) {
-	return getPickStatus(pick, winner, (p, w) => p.team === w.team);
-}
-
-function getGamesStatus(pick, winner, series) {
-	// sometimes we can assign correctness early
-	if (winner === null) {
-		const gamesPlayed = series.totalGames();
-		// since we know the 7th game will be the last we can give points early
-		if (gamesPlayed === 6 && pick.games === 7) {
-			return PickStatus.CORRECT;
-		}
-		// if >= games than the guess have been played, it's a bad guess
-		if (gamesPlayed >= pick.games) {
-			return PickStatus.INCORRECT;
-		}
-		// certain games become impossible, ie both teams win 1 each so 4 games is impossible
-		const minGamesForWinner = Math.min(series.topSeedWins, series.bottomSeedWins) + 4;
-		if (pick.games < minGamesForWinner) {
-			return PickStatus.INCORRECT;
-		}
-	}
-	return getPickStatus(pick, winner, (p, w) => p.games === w.games);
-}
-
-function getPoints(scoring, teamStatus, gamesStatus) {
-	const correctTeam = teamStatus === PickStatus.CORRECT;
-	const correctGames = gamesStatus === PickStatus.CORRECT;
-	let points = 0;
-	points += correctTeam ? scoring.team : 0;
-	points += correctGames ? scoring.games : 0;
-	points += correctTeam && correctGames ? scoring.bonus : 0;
-	return points;
-}
-
-// this function should ONLY be called when there is no winner
-function calculatePossiblePoints(pick, series, scoring, teamStatus, gamesStatus) {
-	const possibleFromTeam = [PickStatus.CORRECT, PickStatus.UNKNOWN].includes(teamStatus) ? scoring.team : 0;
-	const possibleFromGames = [PickStatus.CORRECT, PickStatus.UNKNOWN].includes(gamesStatus) ? scoring.games : 0;
-	const possibleFromBonus = possibleFromTeam > 0 && possibleFromGames > 0 ? scoring.bonus : 0;
-	return possibleFromTeam + possibleFromGames + possibleFromBonus;
 }
---
+++ playoffs/js/pickResultCalculator.js
@@ -0,0 +1,75 @@
+import { PickResult, PickStatus } from './models.js';
+
+export class PickResultCalculator {
+	buildPickResults(scoring, api, picks) {
+		const pickResults = {};
+		for (const [person, picksBySeries] of Object.entries(picks)) {
+			for (const [seriesLetter, pick] of Object.entries(picksBySeries)) {
+				const series = api.getSeries(seriesLetter);
+				const winner = series.getWinner();
+				const teamStatus = this.getTeamStatus(pick, winner);
+				const gamesStatus = this.getGamesStatus(pick, winner, series);
+				const points = this.getPoints(scoring, teamStatus, gamesStatus);
+				const possiblePoints = winner
+					? points
+					: this.calculatePossiblePoints(pick, series, scoring, teamStatus, gamesStatus);
+				if (!pickResults[person]) {
+					pickResults[person] = {};
+				}
+				pickResults[person][series.letter] = PickResult.create({
+					pick: pick,
+					teamStatus: teamStatus,
+					gamesStatus: gamesStatus,
+					points: points,
+					possiblePoints: possiblePoints,
+				});
+			}
+		}
+		return pickResults;
+	}
+
+	getPickStatus(pick, winner, predicate) {
+		if (!winner) {
+			return PickStatus.UNKNOWN;
+		}
+		return predicate(pick, winner) ? PickStatus.CORRECT : PickStatus.INCORRECT;
+	}
+
+	getTeamStatus(pick, winner) {
+		return this.getPickStatus(pick, winner, (p, w) => p.team === w.team);
+	}
+
+	getGamesStatus(pick, winner, series) {
+		if (winner === null) {
+			const gamesPlayed = series.totalGames();
+			if (gamesPlayed === 6 && pick.games === 7) {
+				return PickStatus.CORRECT;
+			}
+			if (gamesPlayed >= pick.games) {
+				return PickStatus.INCORRECT;
+			}
+			const minGamesForWinner = Math.min(series.topSeedWins, series.bottomSeedWins) + 4;
+			if (pick.games < minGamesForWinner) {
+				return PickStatus.INCORRECT;
+			}
+		}
+		return this.getPickStatus(pick, winner, (p, w) => p.games === w.games);
+	}
+
+	getPoints(scoring, teamStatus, gamesStatus) {
+		const correctTeam = teamStatus === PickStatus.CORRECT;
+		const correctGames = gamesStatus === PickStatus.CORRECT;
+		let points = 0;
+		points += correctTeam ? scoring.team : 0;
+		points += correctGames ? scoring.games : 0;
+		points += correctTeam && correctGames ? scoring.bonus : 0;
+		return points;
+	}
+
+	calculatePossiblePoints(pick, series, scoring, teamStatus, gamesStatus) {
+		const possibleFromTeam = [PickStatus.CORRECT, PickStatus.UNKNOWN].includes(teamStatus) ? scoring.team : 0;
+		const possibleFromGames = [PickStatus.CORRECT, PickStatus.UNKNOWN].includes(gamesStatus) ? scoring.games : 0;
+		const possibleFromBonus = possibleFromTeam > 0 && possibleFromGames > 0 ? scoring.bonus : 0;
+		return possibleFromTeam + possibleFromGames + possibleFromBonus;
+	}
+}
