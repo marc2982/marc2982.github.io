154e4d12edde73369373b9c42e81e42172924496
Marc Bryan <marc2982@gmail.com>
2026-03-24 21:21:27 -0600

    Add and configure ESLint

---
+++ playoffs/eslint.config.js
@@ -0,0 +1,22 @@
+module.exports = [
+    {
+        files: ["**/*.js", "**/*.mjs"],
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: "module",
+            globals: {
+                console: "readonly",
+                $: "readonly", // jQuery
+                document: "readonly", // Browser globals
+                window: "readonly",
+                prompt: "readonly",
+                fetch: "readonly",
+                setTimeout: "readonly"
+            }
+        },
+        rules: {
+            "semi": ["warn", "always"],
+            "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }]
+        }
+    }
+];
